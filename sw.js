f88997f8e1ff1ca1bbde7517182967f90e93ad2c
Clark Feusier <cfeusier@gmail.com>
2017-08-18 23:40:33 -0700

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-8577582b40c8ab00e781.js","0acb9bbf78db4a723bc3e351aff1318a"],["/commons-37f2f13296cbdcf76778.js","643950b68d02a68a0842704f2ec1b524"],["/index.html","1da8a813a9beda2e608d964d8ffc4d25"],["/manifest.json","c0a1a7bf44db8283b13e75fe9d18a4c1"],["/offline-plugin-app-shell-fallback/index.html","bec896aec4676570e7773f0f32b80001"]];
+var precacheConfig = [["/app-8577582b40c8ab00e781.js","0acb9bbf78db4a723bc3e351aff1318a"],["/commons-37f2f13296cbdcf76778.js","643950b68d02a68a0842704f2ec1b524"],["/index.html","78d1cf030c9a684aa2ec9b780ffd4dd8"],["/manifest.json","c0a1a7bf44db8283b13e75fe9d18a4c1"],["/offline-plugin-app-shell-fallback/index.html","737b4c396e17896ed26c7a4c124241cc"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
